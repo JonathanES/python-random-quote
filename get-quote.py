9fe1ac9f2b5c7f19f81c513a88da9a979591ae57
JonathanES <jonathan.espiard@epita.fr>
2019-08-25 17:45:28 -0400

    Finish random quote bot

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,38 @@
 import random
 
+
 def primary():
-  #print("Keep it logically awesome.")
+    #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-  last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-if __name__== "__main__":
-  primary()
+    numQuotes = random.randint(0, len(quotes))
+
+    mySet = set()
+    i = 0
+    while len(mySet) < numQuotes:
+      last = len(quotes) - 1
+      rnd = random.randint(0, last)
+      quote = quotes[rnd].rstrip()
+      if quote not in mySet:
+        print(quote)
+        mySet.add(quote)
+      i += 1
+
+def writeQuote(quote):
+    f = open("quotes.txt","a")
+    f.write(quote)
+    f.close()
+
+
+if __name__ == "__main__":
+    print("Please enter a new quote")
+    try:
+      quote = input()
+    except:
+      quote: None
+    if quote != '' and quote != None:
+      writeQuote(quote)
+    primary()
